9454ac7eb9984ec00f8c28456a7769f274eea5a7
Manas <105148415+Manas0726@users.noreply.github.com>
2023-10-15 12:06:03 +0000

    assignment 4

--- PL Assignments/04_assignment.cpp
+++ PL Assignments/04_assignment.cpp
@@ -1,7 +1,40 @@
 #include<iostream>
 using namespace std;
 
+class calculate {
+    int a, b ,c, d;
+    public:
+    calculate() {
+        a = 0;
+        b = 0;
+        c = 0;
+        d = 0;
+    }
+    calculate(int w, int x, int y, int z) {
+        a = w;
+        b = x;
+        c = y;
+        d = z;
+    }
+    calculate(calculate &ref) {
+        a = ref.a;
+        b = ref.b;
+        c = ref.c;
+        d = ref.d;
+    }
+
+    void num() {
+
+        cout <<"radius of circle: " <<3.14159 * a*a;
+    }
+};
+
 int main() {
-    cout <<"Hello world";
+    int radius;
+    cout <<" Enter radius: ";
+    cin >> radius;
+    calculate obj(radius,0,0,0);
+    calculate obj1 = obj;
+    obj.num();
     return 0;
 }
