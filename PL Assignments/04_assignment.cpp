fe0aa0ae490926a08947545a320663e31e977ab7
Manas <105148415+Manas0726@users.noreply.github.com>
2023-10-16 19:12:20 +0000

    assignments

--- PL Assignments/04_assignment.cpp
+++ PL Assignments/04_assignment.cpp
@@ -1,40 +1,81 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class calculate {
-    int a, b ,c, d;
-    public:
+    int a, b;
+
+public:
     calculate() {
         a = 0;
         b = 0;
-        c = 0;
-        d = 0;
     }
-    calculate(int w, int x, int y, int z) {
+
+    calculate(int w, int x) {
         a = w;
         b = x;
-        c = y;
-        d = z;
     }
-    calculate(calculate &ref) {
+
+    calculate(const calculate& ref) {
         a = ref.a;
         b = ref.b;
-        c = ref.c;
-        d = ref.d;
     }
 
-    void num() {
+    void areaCircle() {
+        cout << "Area of Circle: " << 3.14159 * a * a << endl << endl;
+    }
 
-        cout <<"radius of circle: " <<3.14159 * a*a;
+    void areaTriangle() {
+        cout << "Area of Triangle: " << (0.5 * a * b) << endl << endl;
+    }
+
+    void areaSquare() {
+        cout << "Area of Square: " << a * a <<  endl << endl;
     }
 };
 
 int main() {
-    int radius;
-    cout <<" Enter radius: ";
-    cin >> radius;
-    calculate obj(radius,0,0,0);
-    calculate obj1 = obj;
-    obj.num();
+    int radius, length, side;
+    int choice;
+    calculate obj1;
+
+    while (true) {
+        cout << "1. Area of Circle" << endl;
+        cout << "2. Area of Triangle" << endl;
+        cout << "3. Area of Square" << endl;
+        cout << "4. Exit " << endl;
+        cin >> choice;
+
+        if (choice == 4) {
+            break;
+        }
+
+        switch (choice) {
+            case 1:
+                cout << "Enter the radius: ";
+                cin >> radius;
+                obj1 = calculate(radius, 0);
+                obj1.areaCircle();
+                break;
+
+            case 2:
+                cout << "Enter base and height: ";
+                cin >> length >> side;
+                obj1 = calculate(length, side);
+                obj1.areaTriangle();
+                break;
+
+            case 3:
+                cout << "Enter side length: ";
+                cin >> side;
+                obj1 = calculate(side, 0);
+                obj1.areaSquare();
+                break;
+
+            default:
+                cout << "Invalid choice" << endl;
+                break;
+        }
+    }
+
     return 0;
 }
